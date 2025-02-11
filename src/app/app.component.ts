166c02ee42d46e0cbce4410ee55d14a7ae74dd7c
taektu_coding <taektu.coding@gmail.com>
2025-02-11 16:53:34 +0100

    added: custom propertys for the UserComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, UserComponent],
@@ -11,4 +11,5 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   title = 'easy_task';
+  users = DUMMY_USERS; //to impose the data in the template
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
 // get a random nummer of the Array.length Dummy Users
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -25,11 +23,19 @@ export class UserComponent {
   // }
 
   // WITH SIGNAL
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users' + this.selectedUser().avatar);
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+  @Input() avatar!: string; // to set property inside an Component <app-user avatar=" ">
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
